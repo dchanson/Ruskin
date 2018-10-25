cb50ed25a3a085f484d11dcccad7b9f02f34e0e6
Brianna Poche <>
2018-10-25 13:17:46 -0500

    Fix config

--- src/config_template.json.php
+++ src/config_template.json.php
@@ -1,14 +1,13 @@
 <?php
 $r_config = array(
 	"BASE"=> "http://ruskin.local:8080/src",
-	// "BASE"=> "http://147.174.33.197:8080/src",
-	"SCSS_DIRECTORY"=> "../_Resources/css_styles",
+    "SCSS_DIRECTORY"=> "../_Resources/css_styles",
 	"DB"=> array(
 		"HOST"=> "localhost",
 		"DATABASE_NAME"=> "ruskin",
 		"USERNAME"=> "root",
 		"PASSWORD"=> "",
-		"XML_FOLDER"=> "../../_xml",
+		"XML_FOLDER"=> "../../_Completed/",
 		"UPDATE_TOOL_PASSWORD"=> "password"
 	)
 );
