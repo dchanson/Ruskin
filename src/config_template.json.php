e587a33a897cae1f35b43fe9262130da6922eb25
Damodar Dahal <damodar.dahal@selu.edu>
2019-04-15 22:47:52 -0500

    remove old search config

--- src/config_template.json.php
+++ src/config_template.json.php
@@ -1,14 +1,5 @@
 <?php
 $r_config = array(
 	"BASE"=> "http://ruskin.local:8080/src",
-	"SCSS_DIRECTORY"=> "../_Resources/css_styles",
-	"SEARCH_HOST"=> "http://localhost:9001",
-	"DB"=> array(
-		"HOST"=> "localhost",
-		"DATABASE_NAME"=> "ruskin",
-		"USERNAME"=> "root",
-		"PASSWORD"=> "",
-		"XML_FOLDER"=> "../../_Completed/",
-		"UPDATE_TOOL_PASSWORD"=> "password"
-	)
+	"SCSS_DIRECTORY"=> "../_Resources/css_styles"
 );
