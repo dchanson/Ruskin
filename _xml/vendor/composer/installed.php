7100de60c7b5c9887a7b6b22bdbb8b426c2688e4
JayceHammond <hjayce6@gmail.com>
2022-07-15 11:01:01 -0500

    wip: move removal-fix files to php removal

---
+++ _xml/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1b9e3140713623ec587d91e0b53a3505a9041bf9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1b9e3140713623ec587d91e0b53a3505a9041bf9',
+            'dev_requirement' => false,
+        ),
+        'leafo/scssphp' => array(
+            'pretty_version' => 'v0.0.12',
+            'version' => '0.0.12.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../leafo/scssphp',
+            'aliases' => array(),
+            'reference' => 'ff76df3e45af45e808f3fcd516a2cb5cbc77f45e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
